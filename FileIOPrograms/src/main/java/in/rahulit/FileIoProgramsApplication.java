14d20dfbce5496178ec5342e7f00469ca65d564c
itsrahulchaudhary <rahulcy49@gmail.com>
2021-08-05 19:12:38 +0530

    file io

---
+++ FileIOPrograms/src/main/java/in/rahulit/FileIoProgramsApplication.java
@@ -0,0 +1,13 @@
+package in.rahulit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIoProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileIoProgramsApplication.class, args);
+	}
+
+}
